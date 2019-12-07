0cb43584fbb1c3bc421eae1c59b236fe38a7d1bc
Hex-Zero <hex0@live.com>
2019-12-07 21:26:58 +0000

    deprication warning

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,11 @@ app.use(cors());
 app.use(express.json());
 
 const uri = process.env.DB_URI;
-mongoose.connect(uri, { useNewUrlParser: true, useCreateIndex: true });
+mongoose.connect(uri, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useUnifiedTopology: true
+});
 const connection = mongoose.connection;
 connection.once("open", () => {
   console.log("MongoDB database connection established successfully");
