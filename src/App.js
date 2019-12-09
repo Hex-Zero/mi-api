61d4ef9dcaf29f4d91afa56329e9cf9faf91a96e
Hex-Zero <hex0@live.com>
2019-12-09 16:26:44 +0000

    start styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 
 import Navbar from "./components/navbar.component";
