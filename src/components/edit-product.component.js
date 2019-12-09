84a8a3d15fac82223940363a4aa5b20740191de6
Hex-Zero <hex0@live.com>
2019-12-09 16:46:04 +0000

    date picker import

--- src/components/edit-product.component.js
+++ src/components/edit-product.component.js
@@ -1,6 +1,5 @@
 import React, { Component } from "react";
 import axios from "axios";
-import "react-datepicker/dist/react-datepicker.css";
 
 export default class EditProduct extends Component {
   constructor(props) {
